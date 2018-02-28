4b92895ae2e941b7c081d949d110b2a4a82ed6c7
turkonjavla <turkonjavlada@gmail.com>
2018-02-28 22:48:15 +0100

    Changelog #2 Add campgrounds object array to app.js Show campgrounds on index.ejs

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var campgrounds = [
         image: "https://lh6.googleusercontent.com/proxy/OcqYcF8IRbYd5kD9R8dffwVIIQDEYI6pYJGHgNQi_Jvf64vDpNJuM69Z5TR7q37o08indq8V_QS_peThRFoY5DStlL-PSbyG9NdAXwmfY8kMLStZHNpvDIU9ovqRN60FMwF2Qobh7qz1ThSd=s1536-k-no"
     }
 ];
-
+    
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
