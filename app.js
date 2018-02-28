a5b764b06382ea5f53a6baa1210a42ddc72a09bb
turkonjavla <turkonjavlada@gmail.com>
2018-02-28 22:56:03 +0100

    Changelog #2 Add campground object array to app.js (will be switching to mongoDB) Show all campgrounds on index.ejs

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const bodyParser = require("body-parser"),
       app        = express(),
       port       = 5050;
 
+
 var campgrounds = [
     {
-        name: "Yosemite National Park",
+        name: "Yosemite Valley",
         image: "https://lh6.googleusercontent.com/proxy/OcqYcF8IRbYd5kD9R8dffwVIIQDEYI6pYJGHgNQi_Jvf64vDpNJuM69Z5TR7q37o08indq8V_QS_peThRFoY5DStlL-PSbyG9NdAXwmfY8kMLStZHNpvDIU9ovqRN60FMwF2Qobh7qz1ThSd=s1536-k-no"
     }
 ];
-    
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
