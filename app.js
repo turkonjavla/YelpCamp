47622589a4a24431d230b1dd059d6cb261cfb37d
turkonjavla <turkonjavlada@gmail.com>
2018-02-28 21:21:01 +0100

    Initial commit Add express, body-parser and ejs add views folder add landing.ejs add app.js and configure server

---
+++ app.js
@@ -0,0 +1,15 @@
+const bodyParser = require("body-parser"),
+      express    = require("express"),
+      app        = express(),
+      port       = 5050;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("landing");
+});
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`Server started at port ${port}`);
+});
