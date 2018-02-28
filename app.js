9d551bb3dbd124e0f7a54d3b7a2068d752d35b56
turkonjavla <turkonjavlada@gmail.com>
2018-02-28 22:45:02 +0100

    Changelog #2 Add object array 'campgrounds' (will be replaced with mongoDB later) git commit -m Changelog

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const bodyParser = require("body-parser"),
       app        = express(),
       port       = 5050;
 
+var campgrounds = [
+    {
+        name: "Yosemite National Park",
+        image: "https://lh6.googleusercontent.com/proxy/OcqYcF8IRbYd5kD9R8dffwVIIQDEYI6pYJGHgNQi_Jvf64vDpNJuM69Z5TR7q37o08indq8V_QS_peThRFoY5DStlL-PSbyG9NdAXwmfY8kMLStZHNpvDIU9ovqRN60FMwF2Qobh7qz1ThSd=s1536-k-no"
+    }
+];
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
@@ -11,7 +18,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/index", (req, res) => {
-    res.render("index")
+    res.render("index", {campgrounds: campgrounds});
 });
 
 app.listen(process.env.PORT || port, () => {
