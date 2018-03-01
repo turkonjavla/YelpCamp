722e31240dbdba41a4b83be8080b07f327cbb7aa
turkonjavla <turkonjavlada@gmail.com>
2018-03-01 16:28:42 +0100

    Changelog #5 Add description to campground model Add textarea in new ejs for description

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ const bodyParser = require("body-parser"),
       app        = express(),
       port       = 5050;
 
-const db = process.env.DATABASEURL;
+const db = process.env.DATABASEURL; // stored in .env file
 
 mongoose.connect(db)
     .then(() => console.log("MongoDB connected!"))
     .catch(error => console.log("There was an error connecting to MongoDB"));
 
-var campgrounds = [
-    {
-        name: "Yosemite Valley",
-        image: "https://lh6.googleusercontent.com/proxy/OcqYcF8IRbYd5kD9R8dffwVIIQDEYI6pYJGHgNQi_Jvf64vDpNJuM69Z5TR7q37o08indq8V_QS_peThRFoY5DStlL-PSbyG9NdAXwmfY8kMLStZHNpvDIU9ovqRN60FMwF2Qobh7qz1ThSd=s1536-k-no"
-    }
-];
+// campground schema
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -32,7 +34,11 @@ app.get("/", (req, res) => {
 
 // INDEX route
 app.get("/campgrounds", (req, res) => {
-    res.render("index", {campgrounds: campgrounds});
+    Campground.find({})
+        .then(campgrounds => {
+            res.render("index", {campgrounds: campgrounds});
+        })
+        .catch(error => console.log("There was an error displaying campgrounds!"));
 });
 
 // NEW route - show form for creating new campgrounds
@@ -42,11 +48,23 @@ app.get("/campgrounds/new", (req, res) => {
 
 //CREATE campground
 app.post("/campgrounds", (req, res) => {
-    let name  = req.body.name,
-        image = req.body.image;
-    // push campground in array and redirect
-    campgrounds.push({name: name, image: image});
-    res.redirect("/campgrounds");
+    let name        = req.body.name,
+        image       = req.body.image,
+        description = req.body.description,
+        campgrounds = {name: name, image: image, description: description}
+    // Create camprgound and save to DB
+    Campground.create(campgrounds)
+        .then(campground => {
+            res.redirect("/campgrounds");
+            console.log(campground);
+        })
+        .catch(error => console.log("there was an error while adding new campground!"));
+});
+
+// SHOW - shows a specific campground
+app.get("/campgrounds/:id", (req, res) => {
+    // find campground with id
+    // render show page
 });
 
 app.listen(process.env.PORT || port, () => {
